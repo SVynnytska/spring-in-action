d8774c2d27cc12ccfa55d7285716c5037c9596e2
SofiaVynnytska <sonkavyn@gmail.com>
2016-09-19 09:46:39 +0300

    spring in action CHAPTER 1

---
+++ src/main/java/com/springinaction/soundsystem/CompactDisc.java
@@ -0,0 +1,7 @@
+package com.springinaction.soundsystem;
+
+/**
+ * Created by svinnytska on 9/19/2016.
+ */
+public interface CompactDisc {
+}
---
+++ src/main/java/com/springinaction/soundsystem/SgtPeppers.java
@@ -0,0 +1,4 @@
+package com.springinaction.soundsystem;
+
+public class SgtPeppers {
+}
